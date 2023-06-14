e49f256e6e7f75d1a957b49a93d68658d0a99545
joosang425 <dkrehd456@naver.com>
2023-06-14 20:25:32 +0900

    Create project

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <Logo />
+    <a
+      href="https://github.com/joosang425"
+      target="_blank"
+      class="logo">
+      (c){{ new Date().getFullYear() }} JOOSANG
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
